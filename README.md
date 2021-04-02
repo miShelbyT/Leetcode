0de9a9ab6cca6d2a4ba475aece54b1e907138efc
miShelbyT <mi.shelbyrose@gmail.com>
2021-04-01 21:30:17 -0400

    working thru Leetcode array probs

---
+++ README.md
@@ -0,0 +1,3 @@
+A big thanks to Warren Niu for his blog on the xor operator and the accompanying visual on how it works with .reduce():
+https://blog.usejournal.com/find-the-unique-number-in-an-array-using-the-xor-operator-54d35aa9e8d0
+
--- singleNumber.js
+++ singleNumber.js
@@ -4,15 +4,23 @@
 
 // first i will use frequency counter. even tho it does use add'l memory. good practice!
 
-var singleNumber = function(nums) {
-  let numsObj = {}
-  for(let i = 0; i < nums.length; i++){
-    let num = nums[i]
-    numsObj[num] ? numsObj[num] += 1 : numsObj[num] = 1
-  } 
-   for(num in numsObj) {
-     if(numsObj[num] < 2) return num
-   }
-};
+// var singleNumber = function(nums) {
+//   let numsObj = {}
+//   for(let i = 0; i < nums.length; i++){
+//     let num = nums[i]
+//     numsObj[num] ? numsObj[num] += 1 : numsObj[num] = 1
+//   } 
+//    for(num in numsObj) {
+//      if(numsObj[num] < 2) return num
+//    }
+// };
 
-console.log(singleNumber([7,1,2,1,2,7,8]))
+// also using xor operator with zero additional space complexity
+var singleNumber = function(nums) {
+  // reduce compares nums[0] and nums[1] and then compares the result to nums[2] and so on. xor turns numbers into binary format
+  return nums.reduce((a,b) => a ^ b)
+}
+
+
+
+console.log(singleNumber([8,1,2,1,2,7,8]))
